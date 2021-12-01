861b52ce7d9bad42622103c127e5eefbeffc2c24
unknown <Saitharala@gmail.com>
2021-12-02 02:04:31 +0530

    [ Tharala SaiPrashanth] Added  First Name  Feauture

---
+++ UserRegistrationPrograms/UserRegistrationPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UserRegistrationPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration\n");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Validate Program\n 1.First Name\n 2.Exit\n");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        ValidateFN FirstName = new ValidateFN();
+                        Console.WriteLine("Enter the First Name to Check Validation:");
+                        string input = Console.ReadLine();
+                        FirstName.ValidateFirstName(input);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
